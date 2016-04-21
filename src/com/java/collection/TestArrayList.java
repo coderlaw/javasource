ff1ae870313ddcfc750acea16d75ba472f988dbc
coderlaw <xuehuan_luo@outlook.com>
2016-04-21 14:40:15 +0800

    c

---
+++ src/com/java/collection/TestArrayList.java
@@ -0,0 +1,13 @@
+package com.java.collection;
+
+import java.util.ArrayList;
+
+public class TestArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList al = new ArrayList();
+
+	}
+
+}
